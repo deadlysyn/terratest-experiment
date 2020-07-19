ae744219cb5000dea35c662497156405b0538125
Mike Hoskins <mrh@devopsdreams.io>
2020-07-19 19:20:15 -0400

    factor out networking; fix paths after module rename

--- modules/web-task/outputs.tf
+++ modules/web-task/outputs.tf
@@ -1,11 +1,3 @@
-output "vpc_id" {
-  value = data.aws_vpc.selected.id
-}
-
-output "subnet_ids" {
-  value = data.aws_subnet_ids.selected.ids
-}
-
 output "ecr_repository_url" {
-  value = aws_ecr_repository.web.repository_url
+  value = aws_ecr_repository.web_task.repository_url
 }
