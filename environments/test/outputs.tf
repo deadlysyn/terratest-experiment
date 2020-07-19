3a3a31b5693704a086d80c60531f455f35cbed07
Mike Hoskins <mrh@devopsdreams.io>
2020-07-19 19:17:02 -0400

    cleanup

--- environments/test/outputs.tf
+++ environments/test/outputs.tf
@@ -2,14 +2,22 @@ output "availability_zones" {
   value = module.network.availability_zones
 }
 
-output "vpc_id" {
-  value = module.network.vpc_id
+output "ecr_repository_url" {
+  value = module.web_task.ecr_repository_url
+}
+
+output "subnet_cidrs" {
+  value = module.network.subnet_cidrs
 }
 
 output "subnet_ids" {
   value = module.network.subnet_ids
 }
 
-output "ecr_repository_url" {
-  value = module.web_task.ecr_repository_url
+output "vpc_cidr" {
+  value = module.network.vpc_cidr
+}
+
+output "vpc_id" {
+  value = module.network.vpc_id
 }
