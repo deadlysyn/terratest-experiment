1ad190fb64785b03c4a2b7f88dfa01264d406937
Mike Hoskins <mrh@devopsdreams.io>
2020-07-19 17:46:48 -0400

    fix path names

--- environments/test/outputs.tf
+++ environments/test/outputs.tf
@@ -1,11 +1,15 @@
+output "availability_zones" {
+  value = module.network.availability_zones
+}
+
 output "vpc_id" {
-  value = module.web.vpc_id
+  value = module.network.vpc_id
 }
 
 output "subnet_ids" {
-  value = module.web.subnet_ids
+  value = module.network.subnet_ids
 }
 
 output "ecr_repository_url" {
-  value = module.web.ecr_repository_url
+  value = module.web_task.ecr_repository_url
 }
