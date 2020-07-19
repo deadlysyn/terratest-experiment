ace33b64d0563fd8d6214d0fe5d5104daec52ea8
Mike Hoskins <mrh@devopsdreams.io>
2020-07-19 17:46:21 -0400

    factor out network module; auto select task roles

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = "~> 0.12.0"
 
   required_providers {
-    aws = ">= 2.56"
+    aws = "~> 2.42"
   }
 }
 
@@ -12,20 +12,29 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-module "web" {
-  source                 = "../../modules/web"
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  # account_arn = data.aws_caller_identity.current.arn
+  task_role = length(var.task_role) != 0 ? var.task_role : "arn:aws:iam::${local.account_id}:role/ecsTaskExecutionRole"
+  exec_role = length(var.exec_role) != 0 ? var.exec_role : "arn:aws:iam::${local.account_id}:role/ecsTaskExecutionRole"
+}
+
+module "network" {
+  source      = "../../modules/network"
+  region      = var.region
+  vpc_filters = var.vpc_filters
+}
+
+module "web_task" {
+  source                 = "../../modules/web-task"
   app_name               = var.app_name
   container_cpu          = var.container_cpu
   container_memory       = var.container_memory
   ecr_expire_days        = var.ecr_expire_days
   environment            = var.environment
-  execution_role_arn     = var.execution_role_arn
+  exec_role              = var.exec_role
   region                 = var.region
   tags                   = var.tags
-  task_role_arn          = var.task_role_arn
+  task_role              = var.task_role
   web_log_retention_days = var.web_log_retention_days
-  # alb_destroy_log_bucket             = var.alb_destroy_log_bucket
-  # aws_account_id                     = data.aws_caller_identity.current.account_id
-  # aws_account_arn                    = data.aws_caller_identity.current.arn
 }
-
